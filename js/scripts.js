c3f4346d48072584e082112bf7b7e89fa533f8df
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-03-11 17:30:26 -0800

    add initial text to index.html to create a website that takes in a numerical user input and returns an inclusive count up to that number, where numbers divisible by 3 are replaced with 'ping,' numbers divisible by 5 are replaced with 'pong,' and numbers divisible by both 3 and 5 are relaced with 'ping-pong.'

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// business logic
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var checkForVowel = function(letter) {
+  for (var index = 0; index < vowels.length; index += 1) {
+    if (vowels[index] === letter) {
+      var isVowel = true;
+    }
+  }
+  if (isVowel === true) {
+    return true;
+  } else {
+    return false;
+  }
+} // function to check if letter is a vowel, return true/false accordingly
+
+
+
+// front-end logic
