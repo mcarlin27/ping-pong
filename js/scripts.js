a6e6c8fe10dc0d837db34996cae4a5f46d455f17
Marilyn Carlin <marilyn.s.carlin@gmail.com>
2017-03-11 22:02:36 -0800

    add front end logic to accept all user input types, including non-integer numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@
 var pingPong = function(userInput) {
   var numberArray = [];
   for (var number = 1; number <= userInput; number++) {
-    // if (isNaN(userInput)) {
-    //   alert("Please enter a number.");
-    if (userInput % 3 === 0) {
+    if (userInput = NaN) {
+      alert("Please enter a number.");
+    } if (userInput % 3 === 0) {
       numberArray.push("ping");
     } else if (userInput % 5 === 0) {
       numberArray.push("pong");
@@ -13,22 +13,21 @@ var pingPong = function(userInput) {
     } else {
        numberArray.push(number);
     };
-    
-    return numberArray;
   };
+  return numberArray;
 // console.log(numberArray);
 };
 
 
 // front-end logic
 $(document).ready(function() {
-  $("form#number-count").submit(function(event) {
+  $("#number-count").submit(function(event) {
     event.preventDefault();
 
-    var userInput = $("#input").val();
+    var userInput = parseFloat($("#input")).val();
     console.log(userInput);
     var output = pingPong(userInput);
-    console.log(output);
+    // console.log(output);
     // $("#result").text(output);
 
 
